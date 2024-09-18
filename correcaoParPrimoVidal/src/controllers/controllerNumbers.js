15373f31031fa5ac2235d5c015632c9f98f50b82
JoaoPedroVidal9 <jp.joaopedro.franc0@gmail.com>
2024-09-18 08:42:54 -0300

    Exercício Correção

---
+++ correcaoParPrimoVidal/src/controllers/controllerNumbers.js
@@ -0,0 +1,24 @@
+module.exports = class controllerNumbers{
+    static async checkPar(req, res){
+        const {numero} = req.body;
+
+        if(numero%2 == 0){
+            return res.status(200).json({message : "O número é par!"});
+        }
+        return res.status(400).json({message:"O número é ímpar!"});
+    }
+
+    static async checkPrimo(req,res){
+        const {numero} = req.body;
+
+        if(numero<=1){
+            return res.status(200).json({message:"Não é primo, é um ou menos!"});
+        }
+        for(let i = 2; i < numero; i++){
+            if(numero %i == 0){
+                return res.status(200).json({message:"O número não é primo!"});
+            }
+        }
+        return res.status(200).json({message:"É primo!"});
+    }
+}
