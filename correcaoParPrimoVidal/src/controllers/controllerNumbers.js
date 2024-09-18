93f19a4206d3ac02b7d1eb40fea8626ce3f9ec01
JoaoPedroVidal9 <jp.joaopedro.franc0@gmail.com>
2024-09-18 11:58:02 -0300

    Impedir Nan Correção

--- correcaoParPrimoVidal/src/controllers/controllerNumbers.js
+++ correcaoParPrimoVidal/src/controllers/controllerNumbers.js
@@ -2,6 +2,10 @@ module.exports = class controllerNumbers{
     static async checkPar(req, res){
         const {numero} = req.body;
 
+        if(typeof numero !== "number"){
+            return res.status(400).json({message:"Digite um número."})
+        }
+
         if(numero%2 == 0){
             return res.status(200).json({message : "O número é par!"});
         }
@@ -10,6 +14,10 @@ module.exports = class controllerNumbers{
 
     static async checkPrimo(req,res){
         const {numero} = req.body;
+
+        if(typeof numero !== "number"){
+            return res.status(400).json({message:"Digite um número."})
+        }
 
         if(numero<=1){
             return res.status(200).json({message:"Não é primo, é um ou menos!"});
